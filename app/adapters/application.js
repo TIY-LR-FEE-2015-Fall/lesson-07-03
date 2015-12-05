37736e2a3c5637f15e467cfd0e73488eb2fb679f
Ryan Tablada <ryan.tablada@gmail.com>
2015-12-04 22:04:24 -0600

    Allow connect to library from daniel

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   host: 'http://lr-books.herokuapp.com',
+  // host: 'http://librarymanager.apphb.com',
 
   namespace: 'api',
 });
